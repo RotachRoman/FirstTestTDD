114e05d06544dea371363fd9d203766d6ffc4498
RotachRoman <cuck.5@mail.ru>
2020-10-31 13:03:41 +0300

    My first project with tests TDD

---
+++ ToDoApp/Model/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ToDoApp
+//
+//  Created by Rotach Roman on 31.10.2020.
+//
+
+import Foundation
---
+++ ToDoApp/Model/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  ToDoApp
+//
+//  Created by Rotach Roman on 30.10.2020.
+//
+
+import Foundation
